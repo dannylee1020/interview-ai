260c6a2ba1e60ad7fbd033a711004b97a880951e
Daniel Lee <daniel.lee@circleci.com>
2023-10-01 13:02:13 -0700

    add prompt and models

--- app/routers/chat.py
+++ app/routers/chat.py
@@ -1,22 +1,33 @@
 import os
+import json
 
 from fastapi import APIRouter
 from pydantic import BaseModel
 import openai
 
+PROMPT_FILEPATH = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), "..", "..", "prompt", "prompt.json")
+)
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 router = APIRouter()
 
 
-class Prompt(BaseModel):
+class Request(BaseModel):
     message: str
+    model: str = None
 
 
 @router.post("/chat")
-async def send_chat(prompt: Prompt):
+async def send_chat(request: Request):
+    prompt = []
+    with open(PROMPT_FILEPATH, "r") as f:
+        lines = json.load(f)
+    prompt.extend(lines)
+    full_message = prompt + [{"role": "user", "content": request.message}]
+
     completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[{"role": "user", "content": prompt.message}],
+        model=request.model if request.model is not None else "gpt-3.5-turbo",
+        messages=full_message,
         stream=True,
     )
 
