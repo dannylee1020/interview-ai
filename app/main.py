3d4dbfb890801f409179a8b319463d3899c2dd64
Daniel Lee <daniel.lee@circleci.com>
2023-09-30 22:43:14 -0700

    include routers to the app

--- app/main.py
+++ app/main.py
@@ -1,55 +1,15 @@
 import os
-import datetime
-from io import BytesIO
-
-from fastapi import FastAPI, UploadFile, File
-from typing import Annotated
-from pydantic import BaseModel
 import openai
+from fastapi import FastAPI
 
-openai.api_key = os.environ.get("OPENAI_API_KEY")
+from app.routers import chat, transcribe
+
 app = FastAPI()
 
-
-class Prompt(BaseModel):
-    message: str
+app.include_router(chat.router)
+app.include_router(transcribe.router)
 
 
 @app.get("/healthcheck")
 def health_check():
     return {"Hello": "World"}
-
-
-@app.post("/chat")
-def send_chat(prompt: Prompt):
-    completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[{"role": "user", "content": prompt.message}],
-        stream=True,
-    )
-
-    responses = []
-
-    for chunk in completion:
-        responses.append(chunk.choices[0].delta.get("content"))
-    return {"response": responses}
-
-
-@app.post("/transcribe")
-def do_transcribe(audio_file: UploadFile = File(...)):
-    filepath = os.path.join(os.path.dirname(__file__), "files", "audio_file.mp3")
-
-    with open(filepath, "wb") as out_file:
-        data = audio_file.file.read()
-        out_file.write(data)
-
-    contents = open(filepath, "rb")
-    transcript = openai.Audio.transcribe("whisper-1", contents)
-
-    try:
-        os.remove(filepath)
-        print(f"File {filepath} deleted successfully")
-    except OSError as e:
-        print(f"Error deleting {filepath}: {e}")
-
-    return transcript
