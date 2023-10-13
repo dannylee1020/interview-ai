7310f60bb9ff14a7c43fe542f7aa67e81b4c3e07
Daniel Lee <daniel.lee@circleci.com>
2023-10-13 15:30:41 -0700

    testing websocket

---
+++ app/tests/test_ws.py
@@ -0,0 +1,34 @@
+import json
+
+from fastapi.testclient import TestClient
+from app.main import app
+
+
+def test_websocket():
+    client = TestClient(app)
+    with client.websocket_connect("/wc/healthcheck") as websocket:
+        response = websocket.receive_json()
+        websocket.send_json({"message": "from client"})
+        print(response)
+
+
+def test_chat():
+    client = TestClient(app)
+    with client.websocket_connect("/chat") as websocket:
+        websocket.send_json(
+            {
+                "message": "Hello! it's nice to meet you",
+                "model": "gpt-3.5-turbo",
+            }
+        )
+
+        while True:
+            message = websocket.receive_text()
+            if not message:
+                break
+            print(message)
+
+
+if __name__ == "__main__":
+    test_chat()
+    # test_websocket()
