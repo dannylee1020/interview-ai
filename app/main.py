21b07a0c06a1d5185245aa68faa301c671d436e7
Daniel Lee <daniel.lee@circleci.com>
2023-10-13 15:25:55 -0700

    update app

--- app/main.py
+++ app/main.py
@@ -1,15 +1,27 @@
 import os
 import openai
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket
+from fastapi.testclient import TestClient
 
 from app.routers import chat, transcribe
 
+
 app = FastAPI()
 
-app.include_router(chat.router)
+app.add_websocket_route("/testwc", chat.router)
+app.add_websocket_route("/chat", chat.router)
 app.include_router(transcribe.router)
 
 
 @app.get("/healthcheck")
 def health_check():
     return {"Hello": "World"}
+
+
+@app.websocket("/wc/healthcheck")
+async def wc_health_check(websocket: WebSocket):
+    await websocket.accept()
+    await websocket.send_json({"message": "from server"})
+    response = await websocket.receive_json()
+    print(response)
+    await websocket.close()
--- app/routers/chat.py
+++ app/routers/chat.py
@@ -1,8 +1,8 @@
 import os
 import json
 
-from fastapi import APIRouter
-from pydantic import BaseModel
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect
+from fastapi.testclient import TestClient
 import openai
 
 PROMPT_FILEPATH = os.path.abspath(
@@ -12,28 +12,64 @@ openai.api_key = os.environ.get("OPENAI_API_KEY")
 router = APIRouter()
 
 
-class Request(BaseModel):
-    message: str
-    model: str = None
+@router.websocket("/testwc")
+async def test_websocket(websocket: WebSocket):
+    await websocket.accept()
+    await websocket.send_json({"messsage": "sending test message!"})
+    response = await websocket.receive_json()
+    print(f"This is server: {response}")
+    await websocket.close()
 
 
-@router.post("/chat")
-async def send_chat(request: Request):
-    prompt = []
-    with open(PROMPT_FILEPATH, "r") as f:
-        lines = json.load(f)
-    prompt.extend(lines)
-    full_message = prompt + [{"role": "user", "content": request.message}]
+@router.websocket("/chat")
+async def websocket_chat(websocket: WebSocket):
+    try:
+        await websocket.accept()
 
-    completion = openai.ChatCompletion.create(
-        model=request.model if request.model is not None else "gpt-3.5-turbo",
-        messages=full_message,
-        stream=True,
-    )
+        while True:
+            request = await websocket.receive_json()
 
-    responses = []
+            user_message = request.get("message")
+            model = request.get("model", "gpt-3.5-turbo")
+            prompt = []
+            with open(PROMPT_FILEPATH, "r") as f:
+                lines = json.load(f)
+            prompt.extend(lines)
 
-    for chunk in completion:
-        responses.append(chunk.choices[0].delta.get("content"))
+            full_message = prompt + [{"role": "user", "content": request["message"]}]
+            completion = openai.ChatCompletion.create(
+                model=model,
+                messages=full_message,
+                stream=True,
+            )
 
-    return {"response": responses}
+            for chunk in completion:
+                message = chunk.choices[0].delta.get("content")
+                await websocket.send_json(message)
+
+    except WebSocketDisconnect as e:
+        print(f"websocket disconnected: {str(e)}")
+        await websocket.close(code=1000, reason=None)
+    except Exception as e:
+        print(f"Error in Websocket connnection : {str(e)}")
+        await websocket.close(code=1011, reason="internal Error")
+
+
+# connection manager class for handling mutiple client connections
+
+# # class ConnectionManager:
+# #     def __init__(self):
+# #         self.active_connections: list[WebSocket] = []
+
+# #     async def connect(self, websocket: Websocket):
+# #         await websocket.accept()
+# #         self.active_connections.append(websocket)
+
+# #     def disconnect(self, websocket: Websocket):
+# #         self.active_connections.remove(websocket)
+
+# #     async def send_message(self, message: str, websocket: WebSocket):
+# #         await websocket.send_text(message)
+
+
+# # manager = ConnectionManager()
