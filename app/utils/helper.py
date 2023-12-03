cd8826c4897643e1cdf777f3f083501587062946
Daniel Lee <daniellee@example.com>
2023-12-02 21:49:48 -0800

    store util functions

---
+++ app/utils/helper.py
@@ -0,0 +1,10 @@
+import subprocess
+
+import ffmpeg
+
+
+def convert_to_opus(source_path, opus_path):
+    # Run ffmpeg command to convert MP3 to Opus
+    subprocess.run(
+        ["ffmpeg", "-i", source_path, "-b:a", "64k", "-vbr", "on", opus_path]
+    )
