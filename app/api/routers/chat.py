2d48672a399c672b0d07fb40d3e37fbd851253b4
Daniel Lee <dannylee1020@gmail.com>
2023-12-10 22:46:24 -0800

    removes logging and chat endpoint

--- app/api/routers/chat.py
+++ app/api/routers/chat.py
@@ -55,7 +55,6 @@ async def ws_audio_chat(websocket: WebSocket):
 
             # text -> speech
             speech_bytes = await text_to_speech(response)
-            logging.info(context)
             logging.info("Sending data back to client...")
             await manager.send_bytes(speech_bytes, websocket)
 
@@ -64,43 +63,11 @@ async def ws_audio_chat(websocket: WebSocket):
             if len(context) > 25:
                 c = await summarize_context(context)
                 context = c
-                logging.info(context)
     except openai.AuthenticationError as e:
         print("Error authenticating. Check your OpenAI API key")
     except WebSocketDisconnect as e:
         manager.disconnect(websocket)
         context = []
-        await manager.broadcast(f"websocket disconneted {str(e)}")
-    except Exception as e:
-        manager.disconnect(websocket)
-        await manager.broadcast(f"Error in websocket connection: {str(e)}")
-
-
-@router.websocket("/chat")
-async def websocket_chat(websocket: WebSocket):
-    """
-    This endpoint receives messages from websocket,
-    sends the message to the GPT endpoint,
-    and sends the response from GPT back to the client.
-    """
-    logging.info("Opening websocket channel..")
-    await manager.connect(websocket)
-
-    try:
-        while True:
-            message = await websocket.receive_text()
-            logging.info("Received message from client..")
-
-            stream = await chat_completion(message, stream=True)
-
-            logging.info("Sending the message back to the client...")
-            async for part in stream:
-                message = part.choices[0].delta.content or ""
-                await manager.send_message(message, websocket)
-    except openai.AuthenticationError as e:
-        print("Error authenticating. Check your OpenAI API key")
-    except WebSocketDisconnect as e:
-        manager.disconnect(websocket)
         await manager.broadcast(f"websocket disconneted {str(e)}")
     except Exception as e:
         manager.disconnect(websocket)
