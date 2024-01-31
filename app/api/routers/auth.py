376fe9fe0897dd11b4495de9c25207125119107b
Daniel Lee <dannylee1020@gmail.com>
2024-01-30 21:38:09 -0800

    cleanup api spec

--- app/api/routers/auth.py
+++ app/api/routers/auth.py
@@ -48,7 +48,19 @@ class RefreshToken(BaseModel):
     token: str
 
 
-@router.post("/signup", status_code=201)
+class Message(BaseModel):
+    message: str
+
+
+@router.post(
+    "/signup",
+    status_code=201,
+    response_model=Message,
+    responses={
+        500: {"model": Message, "description": "Internal server error"},
+        201: {"model": Message, "description": "Returns signup successful message"},
+    },
+)
 def signup_user(email: Annotated[str, Form()], password: Annotated[str, Form()]):
     user = auth.get_user(email)
     uid = uuid.uuid4()
@@ -83,11 +95,26 @@ def signup_user(email: Annotated[str, Form()], password: Annotated[str, Form()])
         raise HTTPException(status_code=500, detail=f"Internal server error: {e}")
 
 
-@router.post("/login", status_code=201)
+@router.post(
+    "/login",
+    status_code=201,
+    response_model=Token,
+    responses={
+        201: {
+            "model": Token,
+            "description": "Returns access and refresh token",
+        },
+        401: {
+            "model": Message,
+            "description": "Returns HTTP exception for incorrect authentication",
+        },
+    },
+)
 def login_user(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]) -> Token:
     """
-    Validates user credential, then invalidates user's refresh token.
-    Finally returns newly created access and refresh token back to user
+    Validates user credential. When user is verified, invalidates user's
+    refresh token and returns new access and refresh token
+
     """
     user = auth.authenticate_user(form_data.username, form_data.password)
     if not user:
@@ -119,7 +146,14 @@ def login_user(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]) -> To
     )
 
 
-@router.get("/logout", status_code=200)
+@router.get(
+    "/logout",
+    status_code=200,
+    responses={
+        200: {"model": Message, "description": "User successfully logs out"},
+        500: {"model": Message, "description": "Internal server error"},
+    },
+)
 def logout_user(token: Annotated[str, Depends(oauth2_scheme)]):
     d_token = auth.decode_jwt(token, refresh=False)
     # invalidate token by removing from cache
@@ -128,13 +162,25 @@ def logout_user(token: Annotated[str, Depends(oauth2_scheme)]):
     return JSONResponse(content={"message": "user successfully logged out"})
 
 
-@router.post("/token/refresh", status_code=200)
-# def refresh_token(refresh_token: RefreshToken):
+@router.get(
+    "/token/refresh",
+    status_code=200,
+    responses={
+        200: {
+            "model": Token,
+            "description": "Returns a new set of access and refresh token",
+        },
+        401: {
+            "model": Message,
+            "description": "Refresh token is not valid, please login again",
+        },
+    },
+)
 def refresh_token(refresh_token: Annotated[str, Depends(oauth2_scheme)]):
     """
-    check if refresh token exists in the cache server.
-    if token doesn't exist or expired, make user login again.
-    if valid, returns a newly created access and refresh token back to user.
+    Check if refresh token exists in the cache server.
+    If token doesn't exist or expired, redirect user to login.
+    If valid, return new access and refresh token.
     """
     d_token = auth.decode_jwt(refresh_token, refresh=True)
 
@@ -170,17 +216,34 @@ def refresh_token(refresh_token: Annotated[str, Depends(oauth2_scheme)]):
     )
 
 
-@router.post("/reset-password", status_code=201)
+@router.put(
+    "/reset-password",
+    status_code=201,
+    responses={
+        201: {
+            "model": Message,
+            "description": "Returns when password is successfully updated",
+        },
+        500: {"model": Message, "description": "Internal server error"},
+    },
+)
 def reset_password(cred: ResetPassword):
-    pw_hash = auth.hash_password(cred.new_password)
-    auth.verify_password(pw_hash, cred.new_password)
+    try:
+        pw_hash = auth.hash_password(cred.new_password)
+        auth.verify_password(pw_hash, cred.new_password)
 
-    conn = pg_conn.create_db_conn()
-    conn.execute(
-        queries.reset_password, (pw_hash, datetime.now(timezone.utc), cred.email)
-    )
-    conn.commit()
-    conn.close()
+        conn = pg_conn.create_db_conn()
+        conn.execute(
+            queries.reset_password, (pw_hash, datetime.now(timezone.utc), cred.email)
+        )
+        conn.commit()
+        conn.close()
+
+    except Exception as e:
+        return HTTPException(
+            status_code=500,
+            detail=f"Internal server error: {e}",
+        )
 
     return JSONResponse(content={"message": "password updated successfully"})
 
