00c75e57acbd584bf36f2858fa91c3e60d1c6e0b
Daniel Lee <dannylee1020@gmail.com>
2024-02-13 19:19:09 -0800

    use requests to see if the error persists

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,9 +1,10 @@
 import os
 import uuid
 
-import httpx
+# import httpx
+import requests
 
-BASE_URL = "http://localhost:8000/auth"
+BASE_URL = "http://127.0.0.1:8000/auth"
 TEST_DATA = {"email": "test-email@test.com", "password": "testpassword123"}
 TEST_FORM_DATA = {"username": "test-email@test.com", "password": "testpassword123"}
 TEST_OAUTH_DATA = {
@@ -13,18 +14,18 @@ TEST_OAUTH_DATA = {
 }
 TEST_RESET_PW = {"email": "test-email@test.com", "new_password": "newtestpassword123"}
 
-client = httpx.Client()
+# client = httpx.Client()
 
 
 def test_successful_signup_user():
-    res = client.post(BASE_URL + "/signup", data=TEST_DATA)
+    res = requests.post(BASE_URL + "/signup", data=TEST_DATA)
 
     assert res.status_code == 201
     assert res.json() == {"message": "user successfully created"}
 
 
 def test_duplicate_signup_email():
-    res = client.post(BASE_URL + "/signup", data=TEST_DATA)
+    res = requests.post(BASE_URL + "/signup", data=TEST_DATA)
 
     assert res.status_code == 200
     assert res.json() == {"message": "Email already in use"}
@@ -40,7 +41,7 @@ def test_db_insert_signup(db_conn):
 
 
 def test_successful_login(db_conn, redis_conn):
-    res = client.post(BASE_URL + "/login", data=TEST_FORM_DATA)
+    res = requests.post(BASE_URL + "/login", data=TEST_FORM_DATA)
     res_data = res.json()
 
     db_data = db_conn.execute(
@@ -55,7 +56,7 @@ def test_successful_login(db_conn, redis_conn):
 
 
 def test_incorrect_credential_login():
-    res = client.post(
+    res = requests.post(
         BASE_URL + "/login",
         data={"username": "test-email@test.com", "password": "wrongpassword123"},
     )
@@ -65,7 +66,7 @@ def test_incorrect_credential_login():
 
 
 def test_successful_oauth(db_conn, redis_conn):
-    res = client.post(BASE_URL + "/login/oauth", json=TEST_OAUTH_DATA)
+    res = requests.post(BASE_URL + "/login/oauth", json=TEST_OAUTH_DATA)
     res_data = res.json()
 
     db_data = db_conn.execute(
@@ -89,12 +90,12 @@ def test_oauth_db_insert(db_conn):
 
 
 def test_successful_token_refresh():
-    res = client.post(BASE_URL + "/login", data=TEST_FORM_DATA)
+    res = requests.post(BASE_URL + "/login", data=TEST_FORM_DATA)
     res_data = res.json()
     ref_token = res_data["refresh_token"]
 
     headers = {"Authorization": f"Bearer {ref_token}"}
-    res = client.get(BASE_URL + "/token/refresh", headers=headers)
+    res = requests.get(BASE_URL + "/token/refresh", headers=headers)
 
     assert res.status_code == 200
     assert "access_token" in res.json()
@@ -105,28 +106,28 @@ def test_invalid_refresh_token():
     ref_token = uuid.uuid4()
 
     headers = {"Authorization": f"Bearer {ref_token}"}
-    res = client.get(BASE_URL + "/token/refresh", headers=headers)
+    res = requests.get(BASE_URL + "/token/refresh", headers=headers)
 
     assert res.status_code == 401
     assert "refresh token not valid" in res.text
 
 
 def test_refresh_token_is_blacklisted(redis_conn):
-    res = client.post(BASE_URL + "/login", data=TEST_FORM_DATA)
+    res = requests.post(BASE_URL + "/login", data=TEST_FORM_DATA)
     res_data = res.json()
     old_token = res_data["refresh_token"]
 
     headers = {"Authorization": f"Bearer {old_token}"}
-    client.get(BASE_URL + "/logout", headers=headers)
+    requests.get(BASE_URL + "/logout", headers=headers)
 
-    ref_res = client.get(BASE_URL + "/token/refresh", headers=headers)
+    ref_res = requests.get(BASE_URL + "/token/refresh", headers=headers)
 
     assert ref_res.status_code == 401
     assert "please login again" in ref_res.text
 
 
 def test_successful_logout(redis_conn, db_conn):
-    res = client.post(BASE_URL + "/login", data=TEST_FORM_DATA)
+    res = requests.post(BASE_URL + "/login", data=TEST_FORM_DATA)
     data = res.json()
     ref_token = data["refresh_token"]
 
@@ -135,7 +136,7 @@ def test_successful_logout(redis_conn, db_conn):
     ).fetchone()
 
     headers = {"Authorization": f"Bearer {ref_token}"}
-    res = client.get(BASE_URL + "/logout", headers=headers)
+    res = requests.get(BASE_URL + "/logout", headers=headers)
 
     c = redis_conn.get(f"rt:whitelist:{db_res['id']}")
 
@@ -145,7 +146,7 @@ def test_successful_logout(redis_conn, db_conn):
 
 
 def test_successful_reset_password():
-    res = client.put(BASE_URL + "/reset-password", json=TEST_RESET_PW)
+    res = requests.put(BASE_URL + "/reset-password", json=TEST_RESET_PW)
 
     assert res.status_code == 201
     assert res.json() == {"message": "password updated successfully"}
@@ -163,7 +164,7 @@ def test_db_update_reset_password(db_conn):
 
 
 def test_invalid_reset_password():
-    res = client.put(
+    res = requests.put(
         BASE_URL + "/reset-password",
         json={"email": "test-oauth@test.com", "new_password": "testpassword"},
     )
